23566c055377b40582b3395c5f60527c009908f1
Ayrat <ayrat1@mail.ru>
2016-03-02 21:02:28 +0300

    add circle method to select map type

--- app/src/main/java/de/android/mapstodelete/MainActivity.java
+++ app/src/main/java/de/android/mapstodelete/MainActivity.java
@@ -12,12 +12,13 @@ public class MainActivity extends FragmentActivity {
     SupportMapFragment mapFragment;
     GoogleMap map;
     final String TAG = "myLogs";
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        i = 1;
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         map = mapFragment.getMap();
@@ -29,10 +30,27 @@ public class MainActivity extends FragmentActivity {
     }
 
     private void init() {
+
     }
 
 
     public void onClickTest(View view) {
-        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        if (i > 3) {
+            i = 1;
+        }
+        switch (i) {
+            case 1:
+                map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                i++;
+                break;
+            case 2:
+                map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                i++;
+                break;
+            case 3:
+                map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                i++;
+                break;
+        }
     }
 }
